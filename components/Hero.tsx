4bfd15fc22447ac18b54f2cb101baaa4b07769be
gilbertsibuea <87100242+ILMNX@users.noreply.github.com>
2025-03-13 01:46:23 +0700

    create hero for landing page

---
+++ components/Hero.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { use } from "react"
+import { useEffect } from "react"
+import { useMotionValue } from "framer-motion"
+import { animate } from "framer-motion"
+import { useMotionTemplate } from "framer-motion"
+import { motion } from "framer-motion"
+import Image from "next/image"
+import profilePic from "@/public/profile.png"
+import obj from "@/public/object.png"
+
+
+
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"]
+
+export const Hero = () => {
+    const color = useMotionValue(COLORS_TOP[0])
+
+    //to control the effect of the color change
+    useEffect(() => {
+        animate(color,COLORS_TOP,{
+            ease: "easeInOut",
+            duration : 10,
+            repeat: Infinity,
+            repeatType: "mirror"
+        })
+    },[])
+
+    const backgroundImage = useMotionTemplate `radial-gradient(125% 125% at 50% 0%, #000 50%, ${color})`
+    const border = useMotionTemplate `1px solid ${color}`
+    const boxShadow = useMotionTemplate `0px 4px 24px ${color}`
+
+
+    return(
+        <motion.section
+        style={{backgroundImage}}
+        className="relative grid min-h-screen place-content-center overflow-hidden px-4 py-24 text-gray-200"
+        >
+            <div className="z-10 flex flex-col items-center">
+                <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
+                    Open for work
+                </span>
+                <h1 className="text-white/40 text-7xl font-black">Hi, I am</h1>
+                <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black leading-tight text-transparent md:text-7xl"> 
+                    Gilbert Hasiholan
+                </h1>
+                <Image
+                    src={profilePic}
+                    alt ="Profile Picture"
+                    width = {250}
+                />
+                <div className="flex bg-white/10 shadow-xl p-3 rounded-3xl justify-center items-center space-x-2 mb-4">
+                    <Image
+                        src = {obj}
+                        alt = "object"
+                        width={30}
+                        className="rounded-2xl mx-auto "
+                    
+                    />
+                </div>
+
+            </div>
+
+        </motion.section>
+    )
+}
