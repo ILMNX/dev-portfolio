c06dd10384096783b1c5a09d8daf283f23ceb3ae
gilbertsibuea <87100242+ILMNX@users.noreply.github.com>
2025-03-14 00:39:14 +0700

    deployment fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,6 @@ import "./globals.css";
 
 const poppins = Poppins({subsets: ["latin"],weight:["100", "200", "300", "400", "700", "800", "900"] }); 
 
-const geistSans = localFont({
-  src: "/fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "/fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
