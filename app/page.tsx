1a184d4aafa70beb64ff4639fda05f305e6ab7e8
gilbertsibuea <87100242+ILMNX@users.noreply.github.com>
2025-04-28 09:12:46 +0700

    adding analytics and speedinsights

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,14 @@ import { KeyMetrics } from "@/components/KeyMetrics";
 import { Services } from "@/components/Services";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights} from '@vercel/speed-insights/next';
 
 export default function Home() {
   return (
    <>
+      <Analytics />
+      <SpeedInsights/>
       <Navbar />
       <Hero />
       <Stack />
