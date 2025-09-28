004aba621b3023b8044be501f6c18bd772fe39ac
gilbertsibuea <2217051118@students.unila.ac.id>
2025-09-28 23:17:52 +0700

    tailwind purge fix 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
 'use client'
 
 import { Poppins } from "next/font/google"
-import "./globals.css";
+import "./globals.css"
 import ClientWrapper from '@/components/ClientWrapper'
-import { siteConfig, personStructuredData, websiteStructuredData } from "@/lib/metadata";
+import { ThemeProvider } from "next-themes"
+import { siteConfig, personStructuredData, websiteStructuredData } from "@/lib/metadata"
 
-const poppins = Poppins({subsets: ["latin"],weight:["100", "200", "300", "400", "700", "800", "900"] });
-
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "700", "800", "900"],
+})
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en" dir="ltr">
+    <html lang="en" dir="ltr" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/profile.png" />
@@ -23,25 +26,36 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="canonical" href={siteConfig.url} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify(personStructuredData)
+            __html: JSON.stringify(personStructuredData),
           }}
         />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify(websiteStructuredData)
+            __html: JSON.stringify(websiteStructuredData),
           }}
         />
       </head>
       <body className={`${poppins.className} antialiased`}>
-        <ClientWrapper>
-          {children}
-        </ClientWrapper>
+        <ThemeProvider
+          attribute="class" // adds `class="light"` / `class="dark"` to <html>
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ClientWrapper>
+            {children}
+          </ClientWrapper>
+        </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
