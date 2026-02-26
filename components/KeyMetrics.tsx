76157d9956d290b04d64cba01f1cbad5adb7ef52
gilbertsibuea <2217051118@students.unila.ac.id>
2026-02-26 22:34:46 +0700

    redeploy fix

--- components/KeyMetrics.tsx
+++ components/KeyMetrics.tsx
@@ -68,7 +68,7 @@ const metricVariants = {
         scale: 1, 
         rotate: 0,
         transition: {
-            type: "spring",
+            type: "spring" as const,
             stiffness: 120,
             damping: 14
         }
